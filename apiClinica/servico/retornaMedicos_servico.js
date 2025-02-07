9e63766984c341acd9039ac077c06e6a3cd3ac7c
julpereira <julia.p@estudante.ifro.edu.br>
2025-02-07 17:56:39 -0400

    commit

--- apiClinica/servico/retornaMedicos_servico.js
+++ apiClinica/servico/retornaMedicos_servico.js
@@ -2,49 +2,38 @@ import pool from './conexao.js';
 
 export async function retornaMedicos() {
     const conexao = await pool.getConnection();
-    const query = 'SELECT id, nome, telefone, especialidade FROM medicos';
-    try {
-        const medicos = await executaQuery(conexao, query);
-        return medicos;
-    } finally {
-        conexao.release();
-    }
+    const query = 'SELECT id, nome,  telefone, especialidade FROM medicos';
+    const medicos = executaQuery(conexao, query);
+    conexao.release();
+    return medicos;
 }
 
 export async function retornaMedicosNome(nome) {
     const conexao = await pool.getConnection();
-    const query = 'SELECT id, nome, telefone, especialidade FROM medicos WHERE nome = ?';
-    try {
-        const medicos = await executaQuery(conexao, query, [nome]);
-        return medicos;
-    } finally {
-        conexao.release();
-    }
+    const query = 'SELECT id, nome, telefone, especialidade FROM medicos WHERE nome = "' + nome + '"';
+    const medicos = executaQuery(conexao, query);
+    conexao.release();
+    return medicos;
 } 
 
 export async function retornaMedicosID(id) {
     const conexao = await pool.getConnection();
-    const query = 'SELECT id, nome, telefone, especialidade FROM medicos WHERE id = ?';
-    try {
-        const medicos = await executaQuery(conexao, query, [id]);
-        return medicos.length > 0 ? medicos[0] : null; // Retorna o médico ou null se não encontrado
-    } finally {
-        conexao.release();
-    }
+    const query = 'SELECT id, nome, telefone, especialidade FROM medicos WHERE id = ' + id;
+    const medicos = executaQuery(conexao, query);
+    conexao.release();
+    return medicos;
 }
 
 export async function retornaMedicosEspecialidade(especialidade) {
     const conexao = await pool.getConnection();
-    const query = 'SELECT id, nome, telefone, especialidade FROM medicos WHERE especialidade LIKE ?';
-    try {
-        const medicos = await executaQuery(conexao, query, [`%${especialidade}%`]);
-        return medicos;
-    } finally {
-        conexao.release();
-    }
+    const query = 'SELECT id, nome, telefone, especialidade FROM medicos WHERE especialidade like "%' + especialidade + '%"';
+    const medicos = executaQuery(conexao, query);
+    conexao.release();
+    return medicos;
 }
 
-async function executaQuery(conexao, query, params = []) {
-    const resultado_query = await conexao.execute(query, params);
-    return resultado_query[0]; // Retorna o resultado da consulta
+async function executaQuery(conexao,query) {
+    const resultado_query = await conexao.execute(query);
+    const resposta = resultado_query[0];
+    return resposta;
 }
